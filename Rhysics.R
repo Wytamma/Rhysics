7bf3ea8576dd90dcbad97d64a5370c5151671565
Wytamma Wirth <wytamma.wirth@me.com>
2021-06-20 02:27:30 +1000

    :art: make core code

--- Rhysics.R
+++ Rhysics.R
@@ -1,96 +1,98 @@
-library(plotrix)
-
-setClass("Circle", slots=list(position="vector", r="numeric"))
-
-setGeneric("display", function(self) standardGeneric("display"))
-
-setMethod("display", "Circle", function(self) {
-  draw.circle(self@position[1], self@position[2], self@r, col = '#3498db')
-})
-
-setGeneric("update", function(self) standardGeneric("update"))
-
-setMethod("update", "Circle", function(self) {
-  self@position <- self@position + c(runif(1, -0.2, 0.2), runif(1, -0.2, 0.2))
-  return(self)
-})
-
-setup <- function() {
-  plot(-40:0, type="n", bg=NA)
-  title('Rhyisics')
-}
 
 
-R <- "0000000000000000000000000000000000000000
-0000000000000000000000000000000000000000
-0000000000000000000000000000000000000000
-0000000000000000000000000000000000000000
-0000000000000000000000000000000000000000
-0000000000000000000000000000000000000000
-0000000000000000000000000000000000000000
-0000000000000000000000000000000000000000
-0000000000000000000000000000000000000000
-0000000000000000011111111111111110000000
-0000000000000000011111111111111111100000
-0000000000000000011111111111111111100000
-0000000000000000011111111111111111110000
-0000000000000000011111100000011111110000
-0000000000000000011111100000011111110000
-0000000000000000011111100000001111110000
-0000000000000000011111100000011111110000
-0000000000000000011111111111111111100000
-0000000000000000011111111111111111100000
-0000000000000000011111111111111111000000
-0000000000000000011111111111111000000000
-0000000000000000011111111111111100000000
-0000000000000000011111100011111111000000
-0000000000000000111111100001111111000000
-0000000000000000111111100001111111000000
-0000000000000000111111100000111111100000
-0000000000000000011111100000011111110000
-0000000000000000011111100000011111110000
-0000000000000000011111100000001111111000
-0000000000000000011111100000001111111000
-0000000000000000011111100000000111111100"
-
-
-
-l <- c()
-line_count = 0
-char_count = 0
-for (line in strsplit(R, '\n')[[1]]) {
-  print(line)
-  line_count = line_count - 1
-  for (char in strsplit(line, '')[[1]]) {
-    char_count = char_count + 1
-
-    if (char == "1") {
-
-      l <- c(l, new("Circle", position=c(char_count, line_count), r=0.7))
-    }
+createCanvas <- function(width, height, bg = 'white', border =  TRUE) {
+  width <<- width
+  height <<- height
+  par(bg = bg, mar = c(1, 1, 1, 1))
+  plot(
+    0:width,
+    ylim = c(0, height),
+    xlim = c(0, width),
+    type = "n",
+    ylab = "",
+    yaxt = 'n',
+    xaxt = 'n',
+    xlab = "",
+    asp = 1,
+    frame.plot = FALSE
+  )
+  if (border) {
+    rect(0, 0, width, height)
   }
-  char_count = 0
 
 }
-print(l)
-l <- unlist(l)
-draw <- function() {
-  for (i in 1:length(l)) {
-    # update i.e. add vec to pos
-    l[[i]] <<- update(l[[i]])
-    # dysplay i.e. draw
-    display(l[[i]])
-  }
+
+ellipse <- function(xpos, ypos, a, b, col = '#3498db') {
+  plotrix::draw.ellipse(xpos,
+                        ypos,
+                        a = a,
+                        b = b,
+                        col = col)
 }
 
-loop <- function() {
-  while (TRUE) {
-    setup()
+
+loop <- function(number_of_frames) {
+  frames <- c()
+  setup()
+  for (i in 0:number_of_frames) {
     flush.console()
     draw()
+    # frames <- c(frames, recordPlot())
     Sys.sleep(0.1)
   }
 }
 
-loop()
 
+setClass("Vector",
+         slots = list(x = 'numeric',
+                      y = 'numeric'))
+
+setMethod("Ops", signature(e1="Vector", e2="Vector"),
+          function(e1, e2) {
+            e1@x=callGeneric(e1@x, e2@x)
+            e1@y=callGeneric(e1@y, e2@y)
+            validObject(e1)
+            return(e1)
+          }
+)
+setMethod("Ops", signature(e1="Vector", e2="numeric"),
+          function(e1, e2) {
+            e1@x=callGeneric(e1@x, e2)
+            e1@y=callGeneric(e1@y, e2)
+            validObject(e1)
+            return(e1)
+          }
+)
+setMethod("Ops", signature(e1="numeric", e2="Vector"),
+          function(e1, e2) {
+            e2@x=callGeneric(e1, e2@x)
+            e2@y=callGeneric(e1, e2@y)
+            validObject(e2)
+            return(e2)
+          }
+)
+
+
+
+R <- "0011111111111111110000000
+0011111111111111111100000
+0011111111111111111100000
+0011111111111111111110000
+0011111100000011111110000
+0011111100000011111110000
+0011111100000001111110000
+0011111100000011111110000
+0011111111111111111100000
+0011111111111111111100000
+0011111111111111111000000
+0011111111111111000000000
+0011111111111111100000000
+0011111100011111111000000
+0111111100001111111000000
+0111111100001111111000000
+0111111100000111111100000
+0011111100000011111110000
+0011111100000011111110000
+0011111100000001111111000
+0011111100000001111111000
+0011111100000000111111100"
